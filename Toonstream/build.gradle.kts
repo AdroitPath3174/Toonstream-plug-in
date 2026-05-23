e0cfbab1530f785aa15a93f8b7cd0274258f2719
AdroitPath3174 <1234minecraft@atomicmail.io>
2026-05-23 21:39:50 +0530

    Create build.gradle.kts

---
+++ Toonstream/build.gradle.kts
@@ -0,0 +1,13 @@
+version = 1
+
+cloudstream {
+    language = "hi"
+    authors = listOf("Sunil")
+    status = 1
+
+    tvTypes = listOf(
+        "Anime"
+    )
+
+    iconUrl = "https://toonstream.vip/favicon.ico"
+}
